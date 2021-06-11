4944aa55e93b241040e426cc581bb249f4e3c1d8
sbb3391 <sbb33391@gmail.com>
2021-06-10 20:24:25 -0400

    commit tree fe4956baa36e6485a622b955ec4b624e451f2f0c

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "N/A"},
+  {year: "2016", result: "N/A"}
+  //...
+]
 
+function superbowlWin(arr) {
+  let result = arr.find( function(object) {
+    return object.result === "W";
+  })
+
+  return !!result ? result.year : undefined
+}
